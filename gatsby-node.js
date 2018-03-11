7664856e68455d3afe6d90ee7b28b0c81d337d31
enrique.ortuno <enrique@ortuno.net>
2018-03-10 22:24:43 -0500

    Wiring up all markdown file+graphql query+createpage api of gatsby in order to  show only the first blog post

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,37 @@
  */
 
  // You can delete this file if you're not using it
+ const path = require('path');
+
+ exports.createPages = ({boundActionCreators, graphql}) => {
+     const {createPage} = boundActionCreators; //This is a function of gatsby API which allow us create pages on the fly
+
+     const postTemplate = path.resolve('src/templates/posts.js');
+
+     return graphql(`{
+         allMarkdownRemark {
+             edges {
+                 node {
+                     html
+                     id
+                     frontmatter {
+                         path
+                         title
+                     }
+                 }
+             }
+         }
+     }`)
+     .then(res=>{
+         if(res.errors) {
+             return Promise.reject(res.errors);
+         }
+
+         res.data.allMarkdownRemark.edges.forEach(({node}) => {
+             createPage({
+                 path: node.frontmatter.path,
+                 component: postTemplate
+             })
+         });
+     })
+ }
