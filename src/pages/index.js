86c383c2679f6f104bd5eaf5f70b8994a763f467
enrique.ortuno <enrique@ortuno.net>
2018-03-12 20:59:50 -0400

    Showing the list of links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+class IndexPage extends Component {
+  render() {
+    let linksToRender = this._getLinks();
+    //debugger;
+    return (
+      <div>
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        
+        <div>
+              {linksToRender.map((link, index) => (
+               /*  <Link
+                  key={link.id}
+                  updateStoreAfterVote={this._updateCacheAfterVote}
+                  index={index}
+                  link={link}
+                /> */
+                <div key={link.node.id}>
+                <a href={link.node.frontmatter.path}>{link.node.frontmatter.title}</a>
+                <br/>                
+                </div>
+              ))}
+        </div>
+        
+      </div>
+    )
+  }
+
+  _getLinks = () => {
+    //if (isNewPage) {
+    return this.props.data.allMarkdownRemark.edges;
+    /* }
+    const rankedLinks = this.props.feedQueryx.feed.links.slice()
+    rankedLinks.sort((l1, l2) => l2.votes.length - l1.votes.length)
+    return rankedLinks */
+  }
+}
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(limit:100) {
+    edges {
+      node {
+        id
+        frontmatter {
+          title
+          path
+        }
+      }
+    }
+  }
+}
+`
 
 export default IndexPage
